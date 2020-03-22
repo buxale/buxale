1b0db3418d441c2be61f923dbe75bad0cd9b331b
Pascal Rieger <pascal@bracket-design.de>
2020-03-22 12:50:21 +0100

    change "generate api key" button color

--- resources/views/livewire/api-keys.blade.php
+++ resources/views/livewire/api-keys.blade.php
@@ -89,7 +89,7 @@
         <span class="inline-flex rounded-md shadow-sm">
             <button
                 type="button"
-                class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150"
+                class="inline-flex items-center px-3 py-2 border border-gray-300 text-sm leading-4 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:text-gray-800 active:bg-gray-50 transition ease-in-out duration-150"
                 wire:click="generateApiKey"
             >
                 {{__('Neuen API Key generieren')}}
