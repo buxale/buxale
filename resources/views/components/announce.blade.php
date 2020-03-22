604563d42394864b7d1e4289f64ede4e8bb1a150
Vittorio Emmermann <vittorio@cierra.de>
2020-03-22 18:41:12 +0100

    Wordings

--- resources/views/components/announce.blade.php
+++ resources/views/components/announce.blade.php
@@ -3,7 +3,7 @@
         <div class="pr-16 sm:text-center sm:px-16">
             <p class="font-medium text-white">
         <span class="md:hidden">
-          Wir sind in der <span class="font-black">BETA</span>, mit Testdaten.
+          Wir sind in der <span class="font-black">ALPHA</span>, mit Testdaten.
         </span>
                 <span class="hidden md:inline">
           Wir sind noch in der <span class="font-black">BETA!</span> Aktuell sind alle Daten lediglich Testdaten und zur reinen <span
@@ -11,7 +11,7 @@
         </span>
                 <span class="block sm:ml-2 sm:inline-block">
           <a href="https://buxale.io" target="_blank" class="text-white font-bold underline">
-            Learn more &rarr;
+            Mehr erfahren &rarr;
           </a>
         </span>
             </p>
