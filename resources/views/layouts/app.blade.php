698d0e70cda1cf5b654a7672847901a299d76fa4
Vittorio Emmermann <vittorio@cierra.de>
2020-03-22 11:46:26 +0100

    form

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,7 +31,7 @@
     <div class="py-10">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <h2 class="text-3xl font-bold leading-tight text-gray-900">
-                {{ isset($title) ? $title : 'Dashboard'}}
+                {{ isset($title) && $title ? $title : 'Dashboard'}}
             </h2>
         </div>
         <div class="max-w-7xl mt-4 mx-auto sm:px-6 lg:px-8">
