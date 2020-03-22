5cc6a5e78ea09eb7c17149688fcd194466f02674
Vittorio Emmermann <vittorio@cierra.de>
2020-03-22 15:34:02 +0100

    form

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -43,15 +43,12 @@
                          class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg">
                         <div class="py-1 rounded-md bg-white shadow-xs">
                             <a href="#"
-                               class="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out">Your
-                                Profile</a>
-                            <a href="#"
-                               class="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out">Settings</a>
+                               class="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out">Dein
+                                Profil</a>
                             <a href="#"
                                class="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out"
                                onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
-                            >Sign
-                                out</a>
+                            >Ausloggen</a>
                             <form id="logout-form" action="{{ route('logout') }}" method="POST"
                                   style="display: none;">
                                 @csrf
