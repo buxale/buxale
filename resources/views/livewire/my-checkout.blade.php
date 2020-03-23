ff92a536cd6eeefab51ef5525bfe91d6a1dff964
Vittorio Emmermann <vittorio@cierra.de>
2020-03-23 21:56:14 +0100

    Responsiveness fix

--- resources/views/livewire/my-checkout.blade.php
+++ resources/views/livewire/my-checkout.blade.php
@@ -1,4 +1,4 @@
-<div>
+<div class="mx-4 sm:m-0">
     <h2 class="text-lg leading-6 font-medium text-gray-900 mb-3 mt-6">Webhooks</h2>
     <div class="flex flex-col">
         <div class="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
