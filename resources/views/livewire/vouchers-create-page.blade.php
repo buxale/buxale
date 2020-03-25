63e5d91f77c357bf8c54847d4b7e4391c04a1d39
Pascal Rieger <pascal@bracket-design.de>
2020-03-25 19:58:07 +0100

    add cancel button to voucher creation form

--- resources/views/livewire/vouchers-create-page.blade.php
+++ resources/views/livewire/vouchers-create-page.blade.php
@@ -125,6 +125,17 @@
     </div>
     <div class="mt-8 border-t border-gray-200 pt-5">
         <div class="flex justify-end">
+
+            {{-- cancel button--}}
+            <span class="ml-3 inline-flex rounded-md shadow-sm">
+                <a
+                    href="{{route('vouchers.index')}}"
+                    class="inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:text-gray-800 active:bg-gray-50 transition ease-in-out duration-150">
+                        Abbrechen
+                </a>
+            </span>
+
+            {{-- save button --}}
             <span class="ml-3 inline-flex rounded-md shadow-sm">
                 <button
                     type="submit"
