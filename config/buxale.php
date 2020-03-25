e70353f11de5f634ffd45f1653af202bdaea167f
Vittorio Emmermann <vittorio@cierra.de>
2020-03-25 20:30:20 +0100

    Checkout fix 4 checkouts by buxale

--- config/buxale.php
+++ config/buxale.php
@@ -2,5 +2,6 @@
 
 return [
     'fee' => 5,
+    // public open token:
     'main_api_token' => env('MAIN_API_TOKEN', '6LJeH9B4NZKNNBtAQVK4HzHISIvM6Wqmx0RkqM6tfPcrZ4hdtwKg0u9DhlBCmXjlw3coNCOTzo2Y8cq4')
 ];
